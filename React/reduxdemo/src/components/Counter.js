bd0d1625e05286039322ed6694ce8ef0b1ad11a0
Tunahan Aydinoglu <tunahanaydinoglu@outlook.com>
2020-09-30 05:29:02 +0300

    ReduxReady

---
+++ React/reduxdemo/src/components/Counter.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <center><h1>{this.props.counter}</h1></center>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state){
+  return {counter: state.counterReducer}
+};
+
+export default connect(mapStateToProps)(Counter);
---
+++ React/reduxdemo/src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from "redux";
+import counterReducer from "./counterReducer"
+
+const reducers = combineReducers({
+    // counterReducer:counterReducer //ayni ise yarar
+    counterReducer 
+});
+
+export default reducers;
