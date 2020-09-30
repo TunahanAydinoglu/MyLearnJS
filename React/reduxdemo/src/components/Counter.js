8c1323f04506645de19735023e49aa8e5d2660c4
Tunahan Aydinoglu <tunahanaydinoglu@outlook.com>
2020-09-30 20:03:13 +0300

    counterr

--- React/reduxdemo/src/components/Counter.js
+++ React/reduxdemo/src/components/Counter.js
@@ -5,14 +5,16 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <center><h1>{this.props.counter}</h1></center>
+        <center>
+          <h1>{this.props.counter}</h1>
+        </center>
       </div>
     );
   }
 }
 
-function mapStateToProps(state){
-  return {counter: state.counterReducer}
-};
+function mapStateToProps(state) {
+  return { counter: state.counterReducer };
+}
 
 export default connect(mapStateToProps)(Counter);
--- React/reduxdemo/src/components/IncreaseCounter.js
+++ React/reduxdemo/src/components/IncreaseCounter.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {increaseCounter} from '../redux/actions/counterActions';
+
 
 class IncreaseCounter extends Component {
     render() {
         return (
             <div>
-                
+                <center><button onClick={e=>{this.props.dispatch(increaseCounter())}}>1 Arttir</button></center>
             </div>
         );
     }
 }
 
-export default IncreaseCounter;
+
+
+function mapDispatchToProps(dispatch){
+    return{actions:bindActionCreators(increaseCounter,dispatch)}
+}
+
+export default connect(mapDispatchToProps)(IncreaseCounter);
