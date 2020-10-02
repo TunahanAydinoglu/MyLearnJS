ee8942b2572a2fc7a6cc18f9d3c39c3fdc851855
tunahanaydinoglu <tunahanaydinoglu@outlook.com>
2020-10-02 20:56:32 +0300

    hook1

---
+++ React/redux-northwind/src/components/toolbox/TextInput.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+const TextInput = ({ name, label, onChange, placeHolder, value, error }) => {
+  let wrapperClass = "form-group";
+  if (error && error.length > 0) {
+    wrapperClass += " has-error";
+  }
+
+  return (
+    <div className={wrapperClass}>
+      <label htmlFor={name}></label>
+      <div className="field">
+        <input
+          type="text"
+          name={name}
+          className="form-control"
+          placeholder={placeHolder}
+          value={value}
+          onChange={onChange}
+        ></input>
+        {error && <div className="aler alert-danger">{error}</div>}
+      </div>
+    </div>
+  );
+};
+
+export default TextInput;
