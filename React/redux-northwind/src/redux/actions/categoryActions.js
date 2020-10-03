94ef0dd7a2f0d443c289b5118e066482d5b58ee7
Tunahan Aydinoglu <tunahanaydinoglu@outlook.com>
2020-10-03 13:54:36 +0300

    la

--- React/redux-northwind/src/redux/actions/categoryActions.js
+++ React/redux-northwind/src/redux/actions/categoryActions.js
@@ -1,7 +1,7 @@
 import * as actionTypes from "./actionTypes";
 
 export function changeCategory(category) {
-  return { type: actionTypes.CHANGE_CATEGORY, payload: category };
+  return { type: actionTypes.CHANGE_CATEGORY, payload:category };
 }
 
 export function getCategoriesSuccess(categories){
