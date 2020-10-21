c377948ab457721e83687890bb670c94ee05d651
Tunahan Aydinoglu <tunahanaydinoglu@outlook.com>
2020-10-21 16:24:26 +0300

    Ajax-ExternalApi

---
+++ React/index.js
@@ -0,0 +1,30 @@
+
+array.forEach(element => {
+    
+});
+var semih=[
+    'asd',
+    'asdfa',
+    'asdfa',
+    'asdfa',
+    'asdfa',
+    'asdfa',
+];
+
+
+for(i=0;i<semih.length,i++){
+    console.log(semih[i]);
+}
+
+
+
+semih.forEach(sem => console.log(sem))
+
+function semih(sem) {
+    return sem;
+}
+var semih = (sem) => {
+    return sem
+};
+
+
