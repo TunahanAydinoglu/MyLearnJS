ee746491de3030f8c0077ae7bfe6e5bf889174c3
Tunahan Aydinoglu <tunahanaydinoglu@outlook.com>
2020-10-23 02:29:09 +0300

    postUI

--- Projects/webpack-babel-server/src/index.js
+++ Projects/webpack-babel-server/src/index.js
@@ -1,5 +1,5 @@
 import { Request } from "./request";
-import {UI} from "./ui.js";
+import { UI } from "./ui.js";
 
 //Select Element
 const form = document.getElementById("employee-form");
@@ -13,19 +13,44 @@ const request = new Request("http://localhost:3000/employees");
 const ui = new UI();
 
 eventListener();
-function eventListener(){
-    document.addEventListener("DOMContentLoader",getAllEmployees);
+
+function eventListener() {
+  document.addEventListener("DOMContentLoaded", getAllEmployees);
+  form.addEventListener("submit", addEmployee);
 }
 
-function getAllEmployees(){
-    request.get()
-    .then((employees) => console.log(employees))
+function getAllEmployees() {
+  request
+    .get()
+    .then((employees) => ui.addAllEmployeeToUI(employees))
     .catch((err) => console.log(err));
 }
 
+function addEmployee(e) {
+  const employeeName = nameInput.value.trim();
+  const employeeDepartment = departmentInput.value.trim();
+  const employeeSalary = salaryInput.value.trim();
 
-
-
+  if (
+    employeeName === "" ||
+    employeeDepartment === "" ||
+    employeeSalary === ""
+  ) {
+    alert("Lutfen alanlarin hepsini doldurunuz");
+  } else {
+    request
+      .post({
+        name: employeeName,
+        department: employeeDepartment,
+        salary: employeeSalary,
+      })
+      .then((employee) => {
+        ui.addEmployeeToUI(employee);
+      }).catch(err=>console.log(err));
+  }
+  ui.clearInput();
+  e.preventDefault();
+}
 
 // request
 //   .get()
--- Projects/webpack-babel-server/src/ui.js
+++ Projects/webpack-babel-server/src/ui.js
@@ -1,9 +1,44 @@
-export class UI{
-    constructor(){
-        this.employeesList = document.getElementById("employees");
-        this.update = document.getElementById("update");
-        this.nameInput = document.getElementById("name");
-        this.salaryInput = document.getElementById("salary");
-        this.departmentInput = document.getElementById("department");
-    }
+export class UI {
+  constructor() {
+    this.employeesList = document.getElementById("employees");
+    this.update = document.getElementById("update");
+    this.nameInput = document.getElementById("name");
+    this.salaryInput = document.getElementById("salary");
+    this.departmentInput = document.getElementById("department");
+  }
+
+  addAllEmployeeToUI(employees) {
+    let result = "";
+    employees.forEach((emp) => {
+      result += `
+        <tr>
+             <td>${emp.id}</td>
+             <td>${emp.name}</td>
+             <td>${emp.department}</td>
+             <td>${emp.salary}</td>
+             <td><a href="#" id = "update-employee" class= "btn btn-danger">Güncelle</a></td> 
+             <td> <a href="#" id = "delete-employee" class= "btn btn-danger">Sil</a></td>
+         </tr>
+            `;
+    });
+    this.employeesList.innerHTML = result;
+  }
+  addEmployeeToUI(emp){
+      this.employeesList.innerHTML += `
+      <tr>
+           <td>${emp.id}</td>
+           <td>${emp.name}</td>
+           <td>${emp.department}</td>
+           <td>${emp.salary}</td>
+           <td><a href="#" id = "update-employee" class= "btn btn-danger">Güncelle</a></td> 
+           <td> <a href="#" id = "delete-employee" class= "btn btn-danger">Sil</a></td>
+       </tr>
+          `
+  }
+
+  clearInput(){
+      this.nameInput.value="";
+      this.departmentInput.value="";
+      this.salaryInput.value="";
+  }
 }
