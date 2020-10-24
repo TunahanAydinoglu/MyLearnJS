a38c41c9e112a20d29ea70909b263f50e0f11ee3
Tunahan Aydinoglu <tunahanaydinoglu@outlook.com>
2020-10-24 23:44:02 +0300

    server.js

--- ExpressJS/Fundamentals/server.js
+++ ExpressJS/Fundamentals/server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 
+const users = [
+    {id:1,name:"Tuna Aydinoglu",city:"Antalya"},
+    {id:2,name:"Ali Biner",city:"Isparta"},
+    {id:3,name:"Furkan Titiz",city:"Samsun"}
+];
+
 const app = express();
 
 const PORT = 3200;
+
+
+//Get Request
+app.get("/users",(req,res,next)=>{
+    res.json(users);
+});
+
 
 app.listen(PORT,()=>{
     console.log("Server Started:"+PORT);
