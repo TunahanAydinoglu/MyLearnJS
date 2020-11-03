582bbad4d8ec1b57d5bace490afe3e69d24b4ae5
Tunahan <tunahanaydinoglu@outlook.com>
2020-11-03 22:13:58 +0300

    express-async-handler

--- ExpressJS/reastApiTutorial/controllers/auth.js
+++ ExpressJS/reastApiTutorial/controllers/auth.js
@@ -1,33 +1,29 @@
 const User = require("../models/User");
-const CustomError = require('../helpers/errors/CustomError')
+const CustomError = require("../helpers/errors/CustomError");
+const asyncErrorWrapper = require("express-async-handler");
 
-const register = async (req, res, next) => {
+const register = asyncErrorWrapper(async (req, res, next) => {
   //POST DATA
   const name = "Tunahan Aydinoglu";
   const email = "tuna@gmail.com";
   const password = "12367";
 
-  //try catch
-  try {
-    //async await
-    const user = await User.create({
-      name,
-      email,
-      password,
-    });
+  //async await
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
 
-    res.status(200).json({
-      success: true,
-      data: user,
-    });
-  } catch (err) {
-    return next(err);
-  }
-};
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
 
 const errorTest = (req, res, next) => {
   // throw new Error("some error");
-  return next(new CustomError("Custom Error Message",400))
+  return next(new CustomError("Custom Error Message", 400));
 };
 
 module.exports = {
