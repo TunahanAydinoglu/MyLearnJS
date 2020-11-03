e5f1e083fc458ad01a8309f92f009ffad4c6b9c1
Tunahan <tunahanaydinoglu@outlook.com>
2020-11-03 22:04:23 +0300

    CustomErrorHandling

--- ExpressJS/reastApiTutorial/middlewares/errors/customErrorHandler.js
+++ ExpressJS/reastApiTutorial/middlewares/errors/customErrorHandler.js
@@ -1,12 +1,18 @@
+const CustomError = require("../../helpers/errors/CustomError");
+
 const customErrorHandler = (err, req, res, next) => {
+  let customError = err;
 
-    let customError = err;
-    console.log(customError.message,customError.status);
-
-  res
-  .status(400)
-  .json({
+  if (err.name === "SyntaxError") {
+    customError = new CustomError("Unexpected Syntax", 400);
+  }
+  if (err.name === "ValidationError") {
+    customError = new CustomError(err.message, 400);
+  }
+  console.log(customError.message, customError.status);
+  res.status(customError.status || 500).json({
     success: false,
+    message: customError.message,
   });
 };
 
